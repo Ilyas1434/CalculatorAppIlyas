1adebb62389774c984f37e4840879c0e9be0c7e6
Ilyas1434 <113720884+Ilyas1434@users.noreply.github.com>
2023-09-10 15:11:38 -0500

    Created spinner object, and filled it. A
    I need to now:
    - Create an onclick event
    - Make a "listener" for the spinner
    - Add colors according to MD3
    - Adjust UI look overall
    -(If time) add sound to button

--- app/src/main/java/com/example/calculatorproject/MainActivity.java
+++ app/src/main/java/com/example/calculatorproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculatorproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.dropDown);
+        CreateSpinner(spinner);
+
+
+    }
+    // The below block of code fills the dropdown menu, and sets its specific type
+    //SOURCE: CodeEasy on YouTube and some help explaining with ChatGPT
+
+    /**
+     * This method is used to fill the spinner (dropdown) widget with elements using ArrayAdapter
+     * SOURCE: CodeEasy on YouTube.com and ChatGPT for help understanding
+     */
+    public void CreateSpinner(Spinner s) {
+        String[] elems = {"+", "-", "×","÷"};
+        // An ArrayAdapter takes data from the code backend and displays it to UI elements
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, elems);
+        // Sets view to look in a specific layout
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
 }
